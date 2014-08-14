2a0c5f9dadd466642d0218a9874a78c79eb5574a
Jeffrey Hu <hujh14@episcopalacademy.org>
2014-08-13 22:40:07 -0400

    small bug with fixtures and editing

--- server/fixtures.js
+++ server/fixtures.js
@@ -2,18 +2,21 @@ if (Listings.find().count() === 0) {
   Listings.insert({
     address: '100 Apple Street',
     price: '100',
-    owner: 'Jeff'
+    owner: 'Jeff',
+    userId: 'default'
   });
   
   Listings.insert({
     address: '200 Banana Ave',
     price: '123',
-    owner: 'Que'
+    owner: 'Que',
+    userId: 'default'
   });
   
   Listings.insert({
-    address: '123 Cat Drive',
+    address: '300 Cat Drive',
     price: '89',
-    owner: 'Mike'
+    owner: 'Mike',
+    userId: 'default'
   });
 }
