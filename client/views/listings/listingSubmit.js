738d0136381addeeed255d2d0d17523fc075a935
Jeffrey Hu <hujh14@episcopalacademy.org>
2014-08-18 23:00:50 -0400

    autocomplete in the works

--- client/views/listings/listingSubmit.js
+++ client/views/listings/listingSubmit.js
@@ -1,4 +1,23 @@
+Template.listingSubmit.rendered = function(){
+	if (!this.rendered){
+	 var script = document.createElement("script");
+	 script.type = "text/javascript";
+	 script.src = "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places";
+	 document.body.appendChild(script);
+
+	 var script = document.createElement("script");
+	 script.type = "text/javascript";
+	 script.src = "https://maps.gstatic.com/cat_js/maps-api-v3/api/js/17/13/%7Bmain,places%7D.js";
+	 document.body.appendChild(script);
+
+	 this.rendered = true;
+	}
+
+	 var autocomplete = new google.maps.places.Autocomplete((document.getElementById('autocomplete')),{ types: ['geocode'] });
+};
+
 Template.listingSubmit.events({
+
 	'submit form': function(e) {
 		e.preventDefault();
 		
