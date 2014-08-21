af381b2b7a3bf2529a76ceee463be498c249b1bf
Q Kalantary <qkalantary@gmail.com>
2014-08-20 22:27:36 -0400

    starting to use pin api

--- client/views/listings/listings_list.js
+++ client/views/listings/listings_list.js
@@ -5,26 +5,49 @@ Template.listingsList.helpers({
 });
 
 Template.listingsList.rendered = function(){
-  
-  
+ 
+  /*
+  // Zoom to 9 when clicking on marker
+google.maps.event.addListener(marker,'click',function() {
+  map.setZoom(9);
+  map.setCenter(marker.getPosition());
+  });
+*/
 
   GoogleMaps.init(
     {
         'sensor': true, //optional
         //'key': 'MY-GOOGLEMAPS-API-KEY', //optional
-        'language': 'en' //optional
-    }, 
-    function(){
+      'language': 'en' //optional
+   }, 
+
+    function initialize() {
         var mapOptions = {
-            zoom: 13,
-            mapTypeId: google.maps.MapTypeId.SATELLITE
+            zoom: 12,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            streetViewControl: false,
+            center: new google.maps.LatLng(39.948157624395726, -75.16712415771485)
         };
         map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions); 
-        map.setCenter(new google.maps.LatLng( 35.363556, 138.730438 ));
+
+        var marker = new google.maps.Marker({
+            position: map.getCenter(),
+            map: map,
+            title: 'Hello World!'
+        });
+
+        var infowindow = new google.maps.InfoWindow({
+            content:"Hello World!"
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.open(map,marker);
+        });
+
+        //google.maps.event.addDomListener(window, 'window', initialize);
     }
+
+
 );
+  
 }
-
-Template.listingSubmit.events({
-
-});
